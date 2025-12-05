465b048c7a69282a49f327960ddc88e507a415cb
Noego-Accedie <noego.accedie@outlook.com>
2025-12-05 13:28:27 +0800

    deploy2

--- gomamamBTM/vite.config.js
+++ gomamamBTM/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gomamambt/", // 👈 replace with your repo name
+  base: "/gomamambt/",
 });
